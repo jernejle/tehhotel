af276177e73f4f40dd26dc2a9ca99f66a495f64b
Gregor Panic <vision810@gmail.com>
2013-01-02 18:22:24 +0100

    add rest services

---
+++ TehHotel.Contracts/Service/IStrankaRESTService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Threading.Tasks;
+using TehHotel.Contracts.Data;
+
+namespace TehHotel.Contracts.Service
+{
+    [ServiceContract(Name = "StrankaRESTService", Namespace = "http//www.tehhotel.com/")]
+    public interface IStrankaRESTService
+    {
+        [OperationContract]
+        [WebInvoke(Method = "POST", ResponseFormat = WebMessageFormat.Xml, BodyStyle = WebMessageBodyStyle.Bare, UriTemplate = "/")]
+        bool CreateStranka(Stranka s);
+
+        [OperationContract]
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Xml, BodyStyle = WebMessageBodyStyle.Bare, UriTemplate = "/{id}")]
+        Stranka ReadStranka(string id);
+
+        [OperationContract]
+        [WebInvoke(Method = "PUT", ResponseFormat = WebMessageFormat.Xml, BodyStyle = WebMessageBodyStyle.Bare, UriTemplate = "/{id}")]
+        bool UpdateStranka(string id, Stranka s);
+
+        [OperationContract]
+        [WebInvoke(Method = "DELETE", ResponseFormat = WebMessageFormat.Xml, BodyStyle = WebMessageBodyStyle.Bare, UriTemplate = "/{id}")]
+        bool DeleteStranka(string id);
+
+        [OperationContract]
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Xml, BodyStyle = WebMessageBodyStyle.Bare, UriTemplate = "/")]
+        List<Stranka> ListStranka();
+    }
+}
---
+++ TehHotel.Model/StrankaRESTService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TehHotel.Contracts.Data;
+using TehHotel.Contracts.Service;
+using TehHotel.Dal.Models.Dao;
+
+namespace TehHotel.Model
+{
+    class StrankaRESTService : IStrankaRESTService
+    {
+        private StrankaEFDao dao = new StrankaEFDao();
+
+        public bool CreateStranka(Stranka s)
+        {
+            return dao.Create(s);
+        }
+
+        public Stranka ReadStranka(string id)
+        {
+            int idInt = Convert.ToInt32(id);
+            return dao.Read(idInt);
+        }
+
+        public bool UpdateStranka(string id, Stranka s)
+        {
+            int idInt = Convert.ToInt32(id);
+            s.Id=idInt;
+            return dao.Update(s);
+        }
+
+        public bool DeleteStranka(string id)
+        {
+            int idInt = Convert.ToInt32(id);
+            return dao.Delete(idInt);
+        }
+
+        public List<Stranka> ListStranka()
+        {
+            return dao.List();
+        }
+    }
+}
